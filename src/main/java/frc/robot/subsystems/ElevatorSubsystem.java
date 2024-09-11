3ed4742ac90ba3a94fc56c4b9a4ddae439de6182
Heggiehog <nizcat5@gmail.com>
2024-09-10 19:56:02 -0700

    Elevator subsystem uses gear ratios and works better

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
 // import com.ctre.phoenix6.signals.NeutralModeValue;
 // import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
@@ -13,11 +15,13 @@ import frc.robot.Constants.Elevator;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
-    private TalonFX motor;
+    private TalonFX armMotor;
+    private TalonFX elevatorMotor;
     private PIDController controller;
     private double targetHeight;
     private double targetAngle;
-    private double motorPower;
+    private double elevatorMotorPower;
+    private double armMotorPower;
     private double currentHeight;
     private double currentAngle;
     private final ShuffleboardTab ElevatorTab = Shuffleboard.getTab("Elevator");
@@ -25,13 +29,17 @@ public class ElevatorSubsystem extends SubsystemBase {
     /** Creates a new ElevatorSubsystem. */
     public ElevatorSubsystem() {
 
-        motor = new TalonFX(15);
+        elevatorMotor = new TalonFX(Elevator.Ports.ELEVATOR_MOTOR_PORT);
+        armMotor = new TalonFX(Elevator.Ports.ARM_MOTOR_PORT);
 
-        motor.clearStickyFaults();
+        elevatorMotor.clearStickyFaults();
+        armMotor.clearStickyFaults();
+        elevatorMotor.setPosition(0);
+        armMotor.setPosition(0);
+
 
         controller = new PIDController(0.4, 0, 0.0125);
 
-        ElevatorTab.addNumber("Current Motor Power", () -> this.motorPower);
         ElevatorTab.addNumber("Target Height", () -> this.targetHeight);
         ElevatorTab.addNumber("Target Angle", () -> this.targetAngle);
         // ElevatorTab.addNumber("PID output", () -> this.controller);
@@ -45,47 +53,48 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public static double inchesToRotations(double height) {
-        return height / Elevator.GEAR_RATIO;
+        return height / Elevator.ELEVATOR_GEAR_RATIO;
     }
 
     public static double rotationsToInches(double rotations) {
-        return rotations * Elevator.GEAR_RATIO;
+        return rotations * Elevator.ELEVATOR_GEAR_RATIO;
     }
 
-    public static double degreesToRotations(double degrees) {
-        return degrees / Elevator.GEAR_RATIO;
+    public static double armInchesToRotations(double degrees) {
+        return degrees / Elevator.Arm.ARM_GEAR_RATIO;
     }
 
-    public static double rotationsToDegrees(double rotations) {
-        return rotations * Elevator.GEAR_RATIO;
+    public static double rotationsToArmInches(double rotations) {
+        return rotations * Elevator.Arm.ARM_GEAR_RATIO;
     }
 
     public void setTargetHeight(double targetHeight) {
-        this.targetHeight = targetHeight;
+        this.targetHeight = MathUtil.clamp(targetHeight, Elevator.MAX_HEIGHT, 0);
+
     }
 
     public void setTargetAngle(double targetAngle) {
-        this.targetAngle = targetAngle;
+        this.targetAngle = MathUtil.clamp(targetAngle, Elevator.Arm.MAX_ANGLE, 0);
+    }
+
+    public boolean nearTarget(double target, double current) {
+        if (target - 0.5 <= current && current <= target + 0.5) {
+            return true;
+        }
+        return false;
+    }
+
+    public double calculateFeedforward(){
+        return 0; //FIXME
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
-        motorPower = controller.calculate(inchesToRotations(targetHeight), targetHeight);
-        motorPower = controller.calculate(degreesToRotations(targetAngle), targetAngle);
+        elevatorMotorPower = controller.calculate(inchesToRotations(elevatorMotor.getPosition().getValueAsDouble()), targetHeight);
+        armMotorPower = controller.calculate(armInchesToRotations(armMotor.getPosition().getValueAsDouble()), targetAngle);
+        elevatorMotor.setVoltage(MathUtil.clamp(elevatorMotorPower+Elevator.ELEVATOR_FEEDFORWARD, -10, 10));
+        armMotor.setVoltage(MathUtil.clamp(armMotorPower+calculateFeedforward(), -10, 10));
     }
 
-    public boolean nearTargetHeight() {
-        if (targetHeight - 0.5 <= currentHeight && currentHeight <= targetHeight + 0.5) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean nearTargetAngle() {
-        if (targetAngle - 1 <= currentAngle && currentAngle <= targetAngle + 1) {
-            return true;
-        }
-        return false;
-    }
 }
