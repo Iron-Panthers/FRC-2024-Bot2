8e4a14c6007e4348907cf20c1d79132213d1674c
audrey <audreypjohnson@gmail.com>
2024-09-26 19:20:23 -0700

    fixing offsets and swerve module ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -137,24 +137,24 @@ public final class Constants {
      * steer is always left
      * from corner perspective
      *
-     * robot visualization:
-     * |──────────────────────|
-     * │2 10 04 1│
-     * │ 25 24 │
-     * │11 S D 03│
-     * │ D S │
-     * │ │
-     * │ │
-     * │ S D │
-     * │ D S │
-     * │12 |────────| 02│
-     * │ 26 │ │ 27 │
-     * │3 13│ batt │01 4│
-     * |──────┴───┬┬───┴──────|
-     * ││
-     * ││
-     * ▼▼
-     * software front
+     *  robot visualization:
+      |──────────────────────|
+      │4   10          11   3│
+      │  04              03  │
+      │09     S      D     12│
+      │     D          S     │
+      │                      │
+      │                      │
+      │     S          D     │
+      │       D      S       │
+      │08    |────────|    05│
+      │  02  │        │  01  │
+      │2   07│  batt  │06   1│
+      |──────┴───┬┬───┴──────|
+                 ││
+                 ││
+                 ▼▼
+           software front
      */
 
     public static final class Modules {
@@ -180,48 +180,48 @@ public final class Constants {
         public static final SteerRequestType steerRequestType = SteerRequestType.MotionMagic;
       }
 
-      public static final class Module1 { // back left
-        public static final int DRIVE_MOTOR = CAN.at(4, "module 1 drive motor");
-        public static final int STEER_MOTOR = CAN.at(3, "module 1 steer motor");
-        public static final int STEER_ENCODER = CAN.at(24, "module 1 steer encoder");
+      public static final class Module1 { // front right
+        public static final int DRIVE_MOTOR = CAN.at(5, "module 1 drive motor");
+        public static final int STEER_MOTOR = CAN.at(6, "module 1 steer motor");
+        public static final int STEER_ENCODER = CAN.at(1, "module 1 steer encoder");
 
         public static final double STEER_OFFSET =
             IS_COMP_BOT
                 ? 0.29443359375 // comp bot offset
-                : 0.0595703125; // practice bot offset
+                : -0.185302734375; // practice bot offset
       }
 
-      public static final class Module2 { // back right
-        public static final int DRIVE_MOTOR = CAN.at(11, "module 2 drive motor");
-        public static final int STEER_MOTOR = CAN.at(10, "module 2 steer motor");
+      public static final class Module2 { // front left
+        public static final int DRIVE_MOTOR = CAN.at(7, "module 2 drive motor");
+        public static final int STEER_MOTOR = CAN.at(8, "module 2 steer motor");
         public static final int STEER_ENCODER = CAN.at(25, "module 2 steer encoder");
 
         public static final double STEER_OFFSET =
             IS_COMP_BOT
                 ? -0.22705078125 // comp bot offset
-                : 0.262451171875; // practice bot offset
+                : -0.127685546875; // practice bot offset
       }
 
-      public static final class Module3 { // front right
-        public static final int DRIVE_MOTOR = CAN.at(13, "module 3 drive motor");
+      public static final class Module3 { // back right
+        public static final int DRIVE_MOTOR = CAN.at(11, "module 3 drive motor");
         public static final int STEER_MOTOR = CAN.at(12, "module 3 steer motor");
-        public static final int STEER_ENCODER = CAN.at(26, "module 3 steer encoder");
+        public static final int STEER_ENCODER = CAN.at(3, "module 3 steer encoder");
 
         public static final double STEER_OFFSET =
             IS_COMP_BOT
                 ? -0.1884765625 // comp bot offset
-                : -0.20825195312; // practice bot offset
+                : -0.228759765625; // practice bot offset
       }
 
-      public static final class Module4 { // front left
-        public static final int DRIVE_MOTOR = CAN.at(2, "module 4 drive motor");
-        public static final int STEER_MOTOR = CAN.at(1, "module 4 steer motor");
-        public static final int STEER_ENCODER = CAN.at(27, "module 4 steer encoder");
+      public static final class Module4 { // back left
+        public static final int DRIVE_MOTOR = CAN.at(9, "module 4 drive motor");
+        public static final int STEER_MOTOR = CAN.at(10, "module 4 steer motor");
+        public static final int STEER_ENCODER = CAN.at(4, "module 4 steer encoder");
 
         public static final double STEER_OFFSET =
             IS_COMP_BOT
                 ? -0.12744140625 // comp bot offset
-                : -0.3564453125 + 180; // practice bot offset
+                : 0.2939453125; // practice bot offset
       }
     }
 
